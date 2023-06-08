ac6f03868156a00339a68ad58c4649436c8dcfbe
75mdnazmul <75mdnazmulhasan@gmail.com>
2023-06-09 00:35:00 +0600

    Join Us part is created

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import JoinUs from '../JoinUs/JoinUs';
 
 const Home = () => {
     return (
         <div>
-            
+            <JoinUs/>
         </div>
     );
 };
