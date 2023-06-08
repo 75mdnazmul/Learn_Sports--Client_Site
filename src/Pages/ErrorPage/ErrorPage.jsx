180a72d355aee4c6d661afb246734fbb833ccc72
75mdnazmul <75mdnazmulhasan@gmail.com>
2023-06-08 09:33:50 +0600

    Error Page is created

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import img from '../../assets/Error/ErrorPhoto.webp'
+
+const ErrorPage = () => {
+
+    return (
+        <div className=" text-center">
+            <div>
+                < img src={img} alt="" className='w-screen col-sm-12 col-lg-6 ' />
+            </div>
+            <div>
+                <Link to='/'>
+                    <button className='px-5 mb-5 rounded-2 fw-bold border-0 py-2' style={{ color: 'white', backgroundColor: '#04B4AE' }} title="Go to the Home">Back to Home</button>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default ErrorPage;
