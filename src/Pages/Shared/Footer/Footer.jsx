73752b67e1c0c38930a28097f6bc73eb4d53d4a1
75mdnazmul <75mdnazmulhasan@gmail.com>
2023-06-08 14:13:26 +0600

    React icon installed and use

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../../public/logo.webp'
+import { FaClock, FaEnvelope, FaMapMarkerAlt, FaPhoneAlt } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div>
-            <div className="footer p-10 bg-base-200 text-base-content">
+        <div className='  bg-base-200 text-base-content'>
+            <div className="footer container mx-auto p-10 ">
                 <div>
                     <Link to='/' className='flex items-center'>
                         <img className='w-[50px]' src={logo} alt="" />
@@ -15,26 +16,38 @@ const Footer = () => {
                 </div>
                 <div>
                     <span className="font-bold text-2xl text-slate-200">Classes</span>
-                    <a className="link link-hover text-lg">Football</a>
-                    <a className="link link-hover">Cricket </a>
-                    <a className="link link-hover">Badminton </a>
-                    <a className="link link-hover">Tennis</a>
-                    <a className="link link-hover">Swimming</a>
+                    <a className="link link-hover hover:text-[#F7B501] text-lg">Football</a>
+                    <a className="link link-hover hover:text-[#F7B501] text-lg">Cricket </a>
+                    <a className="link link-hover hover:text-[#F7B501] text-lg">Badminton </a>
+                    <a className="link link-hover hover:text-[#F7B501] text-lg">Tennis</a>
+                    <a className="link link-hover hover:text-[#F7B501] text-lg">Swimming</a>
                 </div>
                 <div>
                     <span className="font-bold text-2xl text-slate-200">Useful Links</span>
-                    <a className="link link-hover">Privacy Policy</a>
-                    <a className="link link-hover">Terms and Condition</a>
-                    <a className="link link-hover">Disclaimer</a>
-                    <a className="link link-hover">Support</a>
-                    <a className="link link-hover">FAQ</a>
+                    <a className="link link-hover hover:text-[#F7B501] text-lg">Privacy Policy</a>
+                    <a className="link link-hover hover:text-[#F7B501] text-lg">Terms and Condition</a>
+                    <a className="link link-hover hover:text-[#F7B501] text-lg">Disclaimer</a>
+                    <a className="link link-hover hover:text-[#F7B501] text-lg">Support</a>
+                    <a className="link link-hover hover:text-[#F7B501] text-lg">FAQ</a>
                 </div>
                 <div>
                     <span className="font-bold text-2xl text-slate-200">Address</span>
-                    <a className="link link-hover">icon Jl. Raya Kuta No. 142, Kuta - Badung</a>
-                    <a className="link link-hover">(+88) 01 2345 6789</a>
-                    <a className="link link-hover">learn@sports.com</a>
-                    <a className="link link-hover">06.00 AM - 06.00 PM</a>
+                    <div className='flex'>
+                        <span className='pe-3 text-[#F7B501]'><FaMapMarkerAlt /></span>
+                        <a className="link link-hover hover:text-[#F7B501] text-lg">Jl. Raya Kuta No. 142, Kuta - Badung</a>
+                    </div>
+                    <div className='flex'>
+                        <span className='pe-3 text-[#F7B501]'><FaPhoneAlt /></span>
+                        <a className="link link-hover hover:text-[#F7B501] text-lg">(+88) 01 2345 6789</a>
+                    </div>
+                    <div className='flex'>
+                        <span className='pe-3 text-[#F7B501]'><FaEnvelope /></span>
+                        <a className="link link-hover hover:text-[#F7B501] text-lg">learn@sports.com</a>
+                    </div>
+                    <div className='flex'>
+                        <span className='pe-3 text-[#F7B501]'><FaClock /></span>
+                        <a className="link link-hover hover:text-[#F7B501]">06.00 AM - 06.00 PM</a>
+                    </div>
                 </div>
             </div>
             <div className='text-center py-5 bg-slate-800 text-lg text-slate-300'>
